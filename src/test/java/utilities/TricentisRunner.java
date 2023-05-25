65ddfe82337a9cab85c0532aac26cc07a61cefb1
Sundarraj9641 <111488540+Sundarraj9641@users.noreply.github.com>
2023-05-25 14:34:37 +0530

    Update TricentisRunner.java
    commented by sundarraj

--- src/test/java/utilities/TricentisRunner.java
+++ src/test/java/utilities/TricentisRunner.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-
+//commented by sundarraj
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "feature",
